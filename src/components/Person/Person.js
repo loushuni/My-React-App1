c5f73e11a7897c489cb681bc61399bfd3e2c8c1a
Shuni Lou <loushuni@gmail.com>
2018-12-12 22:24:59 -0800

    validated props for person.js

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,5 +1,6 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import './Person.css';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
     constructor(props) {
@@ -20,11 +21,18 @@ class Person extends Component {
             <div className="Person">
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
             </div>
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default Person;
 
